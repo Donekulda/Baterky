b5505e2329fdb30bc4668c76bfaa841fc1ab6ba7
Donekulda <mikulda@hotmail.cz>
2019-06-11 18:38:58 +0200

    Update arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,16 +1,15 @@
 /*
-// připojení potřebných knihoven
+// připojení potřebných knihoven pro ESP
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 
 // vytvoření proměnných s názvem WiFi sítě a heslem
-const char* nazevWifi = "most";
-const char* hesloWifi = "brezinka";
+const char* nazevWifi = "ssid wifi routeru";
+const char* hesloWifi = "heslo";
 */
 
 #include <SPI.h>
 #include <Ethernet.h>
-
 
 #define VT_PIN A0 
 #define AT_PIN A1
@@ -21,23 +20,22 @@ byte mac[] = {
 
 char server[] = "eshop.aeko.cz";
 
-int MyRelay = 7;
-boolean RelayOn;
+int MyRelay = 7; //Pin relé
+boolean RelayOn; // Pokud relé je High(zapnuté) tak se vybijí baterie, důvod je ten aby se nepřevibíjela baterie
+boolean VoltMeterOnly = true; //pokud je na true tak se sepne rele neměří se proud a baterie se nevybijí, pokud false tak se přepne relé a baterie se vybíji a měří se proud
+
+unsigned long time; //Zapisuje se semka hodnota posledního měření v milisekudách
+unsigned long hours = 0; //proměná na které je zapsána hodnota z převodu milisekund na hodiny
 
 //WiFiClient client;
 EthernetClient client;
-//Client client(server, 80);
 
-void setup(void) {
-  // zahájení komunikace po sériové lince
+void setup(void) {  
   pinMode(MyRelay, OUTPUT);
-  digitalWrite(MyRelay, HIGH);
+  digitalWrite(MyRelay, LOW); //vypnutí relé z důvodu zabránění vybíjení baterie když ještě není připojeno arduino k internetu
   RelayOn = true;
   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-  /* Použit před  kvůli technickým potížímtím než jsem musel změnit arduino z arduino WeMos na Arduino Uno
+  /* Použit předtím(při používání ESP), kvůli technickým potížímtím než jsem musel změnit arduino z arduino WeMos na Arduino Uno
   // zahájení komunikace po WiFi s připojením
   // na router skrze zadané přihl. údaje
   WiFi.begin(nazevWifi, hesloWifi);
@@ -75,6 +73,11 @@ void setup(void) {
   // print your local IP address:
   Serial.print("My IP address: ");
   Serial.println(Ethernet.localIP());
+  if(!VoltMeterOnly){
+    digitalWrite(MyRelay, HIGH); //Sepne se relé aby začala se vybíjet baterie
+    delay(1000);
+  }
+  ////time = millis(); //Získá si čas od sepnutí relé pro zvýšení přesnosti měření, bohužel pár prvních sekund nám vytváří nepřesnost 
 
 }
 
@@ -83,6 +86,11 @@ void loop(void) {
     case 1:
       //renewed fail
       Serial.println("Error: renewed fail");
+      if(!VoltMeterOnly){
+        digitalWrite(MyRelay, LOW); //Vypne se vybíjení baterie a počká se 2 sekundy protože relé má nějaké spoždění
+        delay(1000);
+        ////time = millis();
+      }
       break;
 
     case 2:
@@ -91,11 +99,21 @@ void loop(void) {
       //print your local IP address:
       Serial.print("My IP address: ");
       Serial.println(Ethernet.localIP());
+      if(!VoltMeterOnly){
+        digitalWrite(MyRelay, HIGH); //Vypne se vybíjení baterie a počká se 2 sekundy protože relé má nějaké spoždění
+        delay(1000);
+        ////time = millis();
+      }
       break;
 
     case 3:
       //rebind fail
       Serial.println("Error: rebind fail");
+      if(!VoltMeterOnly){
+        digitalWrite(MyRelay, LOW); //Vypne se vybíjení baterie a počká se 2 sekundy protože relé má nějaké spoždění
+        delay(1000);
+        ////time = millis();
+      }
       break;
 
     case 4:
@@ -104,11 +122,27 @@ void loop(void) {
       //print your local IP address:
       Serial.print("My IP address: ");
       Serial.println(Ethernet.localIP());
+      if(!VoltMeterOnly){
+        digitalWrite(MyRelay, HIGH); //Vypne se vybíjení baterie a počká se 2 sekundy protože relé má nějaké spoždění
+        delay(1000);
+        ////time = millis();
+      }
       break;
 
     default:
       //nothing happened
       break;
+  }
+  
+  if(!RelayOn && !VoltMeterOnly){ //Důvod je kvůli tomu aby se otestovalo že baterie byla vyměněna/výjmuta
+    digitalWrite(MyRelay, HIGH);
+    RelayOn = true;
+    delay(1000);
+    //////time = millis();
+  }
+  
+  if(VoltMeterOnly){
+    digitalWrite(MyRelay, LOW); //Když se vypne rele tak se měří jenom napětí 
   }
   
   float vt_read = analogRead(VT_PIN);
@@ -118,7 +152,10 @@ void loop(void) {
   float voltage = vt_read * (5.0 / 1024.0) * 5.0;
   float current = at_read * (5.0 / 1024.0);
   float watts = voltage * current;
-  float wattHour = watts * 0.00833333333;
+  //hours = time/3600000; //nezdařený pokus počítat pomocí milisekund od startu arduina
+  //float wattHour = watts * hours; //miliseconds to hours
+  //time = millis();
+  float wattHour2 = watts * 0.00833333333;
 
   Serial.println();
   Serial.print("Volts: "); 
@@ -127,27 +164,28 @@ void loop(void) {
   Serial.print(current,3);
   Serial.print("\tWatts: ");
   Serial.println(watts,3);
-  Serial.print("\tWattHour: ");
-  Serial.println(wattHour,3);
+  //Serial.print("\tWattHour: ");
+  //Serial.print(wattHour,9);
+  //Serial.println();
+  Serial.print("\tWattHour2: ");
+  Serial.print(wattHour2,9);
+  Serial.println();
+  //Serial.print("\tPocet hodin od posledniho měření: ");
+  //Serial.print(hours);
   Serial.println();
 
-  if(!RelayOn){
-    digitalWrite(MyRelay, HIGH);
-    RelayOn = true;
-    delay(3000);  
-  }
   if (voltage > 2.5){
     if (client.connect(server, 80)) {
       Serial.println("Connected");
       client.print("GET /baterky/baterky_zapis.php?tabulka=");
-      client.print("fialova&Napeti=");
+      client.print("stav&Napeti=");
       client.print(voltage);
       client.print("&Proud=");
       client.print(current);
       client.print("&Watt=");
       client.print(watts);
       client.print("&Wh=");
-      client.print(wattHour);
+      client.print(wattHour2);
       client.println(" HTTP/1.1");
       client.println("Host: eshop.aeko.cz");
       client.println("Connection: close");
@@ -172,9 +210,11 @@ void loop(void) {
     RelayOn = false;
   }
 
-  if(voltage != 0){
+  if(voltage != 0){ //Pokud baterie není zapnutá tak kontroluj v intervalu 5 sekund
     delay(30000);
-  }else{
+  }else if(VoltMeterOnly){
+    delay(15000);
+  }else{  
     delay(5000);
   }
 }

a82dbbc79c69fe86c49df85d2f3015cf037579ec
Donekulda <mikulda@hotmail.cz>
2019-06-11 08:23:14 +0200

    update

--- web/baterky_zapis.php
+++ web/baterky_zapis.php
@@ -4,16 +4,17 @@
 	//$kam = $_GET['druh'];
 	if(isset($_GET['Napeti']) && isset($_GET['Proud']) && isset($_GET['Watt'])){
 		$napeti = $_GET['Napeti'];
-		$proud = $_GET['Proud'];
-		$watt = $_GET['Watt'];
+		if(isset($_GET['Proud'])){ $proud = $_GET['Proud']; }else{$proud = 0.0;}
+		if(isset($_GET['Watt'])){ $watt = $_GET['Watt']; }else{$watt=0.0;}
 		$tabulka = $_GET['tabulka'];
+		if(isset($_GET['Wh'])){ $Wh = $_GET['Wh']; }else{ $Wh = 0.0; }
 	}elseif (isset($_POST['data'])) {
 		$data = (float)$_POST['data'];
 	}else{
 		echo "data not recived";
 	}
 
-	$sql = "INSERT INTO ".$tabulka." (I, U, W) VALUES (".$proud.",".$napeti.",".$watt.")";
+	$sql = "INSERT INTO ".$tabulka." (I, U, W, Wh) VALUES (".$proud.",".$napeti.",".$watt.",".$Wh.")";
 	if ($conn->query($sql) === TRUE) {
     	echo "New record created successfully";
 	} else {
---
+++ web/index.php
@@ -0,0 +1,92 @@
+﻿<?php
+    include "./config.php";
+  
+	$value = 0;
+  $Wh = 0;
+    $druh="modra";
+	if(isset($_GET['druh'])){
+		$druh = $_GET['druh'];
+		if($druh == "Modrá"){
+			$druh = "modra";
+		}else if($druh == "Fialová"){
+			$druh = "fialova";
+		}else if($druh == "Zelená"){
+			$druh = "zelena";
+		}else if($druh == "Žlutá"){
+			$druh = "zluta";
+		}
+	}   
+	
+	if(isset($druh)){
+        $volts = 0.0;
+       
+        $sql = "SELECT U FROM stav WHERE DATE_ADD(cas, INTERVAL 5 MINUTE) >= NOW()";  
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+          // output data of each row
+          $i = 1;
+          while($row = $result->fetch_assoc()) {
+            if($i == $result->num_rows){
+                $volts = $row['U'];
+            }  
+          }
+        }
+        
+        $sql2 = "SELECT MAX(U), Min(U), COUNT(Wh), COUNT(W*0.0166666667) AS WattHour FROM ".$druh;
+        $result2 = $conn->query($sql2);
+        
+        if ($result2->num_rows > 0) {
+          // output data of each row
+          while($row = $result2->fetch_assoc()) {
+            $max = $row['MAX(U)'];    
+            $min = $row['Min(U)'];
+            $Wh = $row['COUNT(Wh)'];
+            if($Wh == 0 or $Wh == null or !isset($Wh))
+              $Wh = $row['WattHour'];
+            $delic = ($max-$min); //hodnota pro deleni voltu 
+            $volts-=$min;
+            if($volts >= 0){
+              $volts*=100;
+              if($delic == 0)
+                  $value=0;
+              else 
+                  $value = ($volts)/$delic;
+            }else
+                $value = 0;
+          }
+        } 	
+	}
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+  <link rel="stylesheet" type="text/css" href="css/tymovky_nabytost.css" media="all">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="refresh" content="15">
+
+</head>
+<body>
+<div>
+	<form method="Get" action="">
+		<input type="submit" name="druh" value="Modrá">
+		<input type="submit" name="druh" value="Fialová">
+		<input type="submit" name="druh" value="Zelená">
+		<input type="submit" name="druh" value="Žlutá">
+	</form>
+</div>
+
+<div>
+
+    <progress max="100" value="<?php echo $value; ?>" class="html5">
+    </progress>
+
+    <p><strong><?php echo ($value > 0)?"Baterie má ".$value."%.":"Baterie není připojená nebo je vybitá!"; ?></strong></p>
+    <p><strong>Tato baterie má kapacitu <?php echo $Wh." Watt Hodin."; ?> </strong></p>
+
+</div>
+</body>
+</html>
